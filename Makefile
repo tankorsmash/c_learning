9b1b63b1eb293d83834aecf494d1710cde1f2e60
tankorsmash <tankorsmash@gmail.com>
2024-10-21 21:03:21 -0400

    add Makefile for running, and not watching, since make seems to quit at first error

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
+.PHONY: main
 main: src/main.c
-	clang -o main src/main.c
+	clang -o main \
+		-Wall -Wpedantic -Werror -Wformat \
+		-fdiagnostics-color=always \
+		src/main.c
 
-
+.PHONY: run
 run: main
 	./main
 
+
+.PHONY: watch
+watch: main
+	watch -n 0.01 -c make run
--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Person {
+	char name[20];
+	int age;
+};
+
+int add(int a, int b) {
+	return a + b;
+}
 
 int main() {
-	printf("Hello, World!!\n");
+	struct Person p;
+	p.age = 10;
+	printf("Hello, World: %d!!\n%s", add(1, 2), p);
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", i);
+	}
 	return 0;
 }
